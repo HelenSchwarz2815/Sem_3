3e587c7727a1aed89dda9ca3aaa7240a1ffcf713
Elena_Parvatkina <e.parvatkina2815@gmail.com>
2022-12-17 20:11:59 +0300

    третий

---
+++ Sem_3/Program.cs
@@ -0,0 +1,52 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом.
+//Console.WriteLine($"введите пятизначное число ");
+//int num = Convert.ToInt32(Console.ReadLine()); 
+//int x = num;
+//int x1 = num / 10000;
+//int x2 = (num - (x1 * 10000)) / 1000;
+//int x3 = (num - (x1 * 10000) - (x2 * 1000)) / 100;
+//int x4 = (num - (x1 * 10000) - (x2 * 1000) - (x3 * 100)) / 10;
+//int x5 = num % 10;
+//if (x5 == x1 && x4 == x2) Console.WriteLine($"число палиндром ");
+//else Console.WriteLine($"число не является палиндромом ");
+
+//Напишите программу, которая принимает на вход число (N) и выдаёт 
+//таблицу кубов чисел от 1 до N.
+//Console.WriteLine($"введите целое число ");
+//int N = Convert.ToInt32(Console.ReadLine()); 
+//Console.WriteLine($"куб чисел равен ");
+//for (int i = 1; i <= N; i++)
+//{
+ //int sqrt = (i * i * i);
+ //Console.WriteLine($"{sqrt} ");
+//}
+
+//Напишите программу, которая принимает на вход координаты двух точек и 
+//находит расстояние между ними в 3D пространстве.
+//double Res(double  x1, double  x2, double  y1, double  y2, double  z1, double  z2)
+//{
+ //double distanse = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+ //return distanse;
+//}
+//try
+//{
+    //Console.Write($"введите координаты x1 ");
+    //double x1 = Convert.ToDouble(Console.ReadLine());
+    //Console.Write($"введите координаты x2 ");
+    //double x2 = Convert.ToDouble(Console.ReadLine());
+    //Console.Write($"введите координаты y1 ");
+    //double y1 = Convert.ToDouble(Console.ReadLine());
+    //Console.Write($"введите координаты y2 ");
+    //double y2 = Convert.ToDouble(Console.ReadLine());
+    //Console.Write($"введите координаты z1 ");
+    //double z1 = Convert.ToDouble(Console.ReadLine());
+    //Console.Write($"введите координаты z2 ");
+    //double z2 = Convert.ToDouble(Console.ReadLine());
+
+    //Console.WriteLine(Res (x1, x2, y1, y2, z1, z2));
+//}
+//catch
+//{
+    //Console.WriteLine($"Введены некорректные данные ");
+//}
